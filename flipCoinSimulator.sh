17770a74177446cdfb01a0cab397204a8939b99b
“PriyankaWadh” <wadhavanepriyanka@gmail.com>
2021-01-20 09:01:29 +0530

    Show number of times head and tail won

--- flipCoinSimulator.sh
+++ flipCoinSimulator.sh
@@ -1,8 +1,13 @@
 #! /bin/bash
-if [ $(( RANDOM%2 )) -eq 0 ]
-then
-        echo Head
-else
-        echo Tail
-fi
-
+head1=0
+tail1=0
+for (( i=0; i<20; i++ ))
+do
+	if [ $(( RANDOM%2 )) -eq 0 ]
+	then
+    	    head1=$(( head1+1 ))
+	else
+            tail1=$(( tail1+1 ))
+	fi
+done
+echo "Head won "$head1 " times and Tail won "$tail1" times"
